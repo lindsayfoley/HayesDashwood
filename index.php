cd5f3209bcda4133c54bd951ef83a2d8850d5e4e
Lindsay Prahalad <lprahalad@hotmail.co.uk>
2015-02-14 21:00:17 +0000

    start of hayes mock up
    Homepage mock up

---
+++ index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<title>E Hayes Dashwood Foundation</title>
+
+<meta name="description" content="E Hayes Dashwood Foundation">
+
+<meta charset="UTF-8">
+
+<link rel="stylesheet" type="text/css" href="style/fallback.css">
+<link rel="stylesheet" type="text/css" href="style/all_styles.css">
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="/js/all_scripts.js" ></script>
+
+<!--[if lt IE 9]>
+
+<script>
+document.createElement('header');
+document.createElement('main');
+document.createElement('nav');
+document.createElement('footer');
+document.createElement('small');
+</script>
+
+<![endif]-->
+
+</head>
+
+<body>
+
+<header>
+	<div>
+		<h1>The E Hayes Dashwood Foundation</h1>
+		<h2>Accommodation for disabled ex-service people</h2>
+	</div>
+	<nav>
+		<ul id="mainNav">
+			<li><a href="\">Home</a></li>
+			<li><a href="about.html">About Us</a></li>
+			<li><a href="services.html">Our Services</a>
+				<ul id="services" class="subNav">
+					<li><a href="guests.html">Guests</a></li>
+					<li><a href="tenants.html">Tenants</a></li>
+					<li><a href="location.html">Location</a></li>
+				</ul>
+			</li>
+			<li><a href="information.html">Information</a>
+				<ul id="info" class="subNav">
+					<li><a href="estate_management.html">Estate Management</a></li>
+					<li><a href="trustees_governors.html">Trustees &amp; Governors</a></li>
+					<li><a href="reports.html">Reports</a></li>
+					<li><a href="useful_links.html">Useful Links</a></li>
+				</ul>
+			</li>
+		</ul>
+	</nav>
+</header>
+
+<main>
+	<section>
+		<img src="/images/hp_hero_4.jpg" alt="the foundation properties" />
+		<div id="purpose">A charitable trust for the provision of almshouse accommodation for ex-Service and<br> ex-Mercantile Marine personnel who suffer infirmity.</div>
+	</section>
+	<section>
+		<p>The Foundation owns 150 one and two bedroomed flats in Telferscot and Radbourne Roads, Balham, South London. These are quiet, pleasant roads in an attractive
+		  residential area not far from Tooting Bec Common and other amenities. Click <a href="#">here</a> to see exactly where they are.</p>
+		<p>100 flats are currently made available to disabled ex-Service people (including the Merchant Marine) who are termed Guests of the Foundation and pay a subsidized 
+		maintenance charge for the accommodation. Click <a href="#">here</a> for details of those who qualify for Guest status and how to apply.</p>
+		<p>All Guests must be capable of independent living. The Foundation does not provide any care services and there is no warden on the estate.</p>
+		<p>The remaining 50 flats are either held on lease or let on the open market mainly on Assured Shorthold Tenancies. No charitable aspect applies to these lettings.
+		 Click <a href="#">here</a> for further details.</p>
+		 <p>Rental income and grants from other ex-Service charities are used to maintain the Estate to a high standard and carry out long-term improvement works.</p>
+	</section>
+</main>
+
+<footer>
+	<small>&#169; E HAYES DASHWOOD FOUNDATION 2016</small>
+	<small>Registered charity number 246460</small>
+	<div class="endOfSection"></div>
+</footer>
+
+</body>
+</html>	
